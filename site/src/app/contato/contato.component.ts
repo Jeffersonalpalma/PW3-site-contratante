f91cd6b3bf01894c76f7a7bb6809115d3949f5a1
lary17181 <139398675+lary17181@users.noreply.github.com>
2025-06-21 18:38:56 -0300

    Finalizado

--- site/src/app/contato/contato.component.ts
+++ site/src/app/contato/contato.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-contato',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './contato.component.html',
-  styleUrl: './contato.component.css'
+  styleUrls: ['./contato.component.css']
 })
 export class ContatoComponent {
+   formContato:FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formContato = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mensagem: ['', Validators.required],
+    });
+  }
+
+  enviarMensagem() {
+  if (this.formContato.valid) {
+    console.log('Mensagem enviada:', this.formContato.value);
+    alert('Mensagem enviada com sucesso!');
+    this.formContato.reset();
+
+  } else {
+    alert('Por favor, preencha todos os campos corretamente.');
+  }
+}
 
 }

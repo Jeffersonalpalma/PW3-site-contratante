ab30137b2197d09e3c7ceea4cb034f46fbbe7b55
lary17181 <139398675+lary17181@users.noreply.github.com>
2025-06-21 20:52:55 -0300

    Heather Pronto

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -5,14 +5,15 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ContatoComponent } from './contato/contato.component';
 import { SobreComponent } from './sobre/sobre.component';
+import { HeatherComponent } from "./heather/heather.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, SobreComponent, CadastroComponent, HomepageComponent],
+  imports: [RouterOutlet, LoginComponent, SobreComponent, CadastroComponent, HomepageComponent, HeatherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'site';
+  title = 'site contratante';
 
 }
---
+++ site/src/app/heather/heather.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-heather',
+  imports: [RouterModule],
+  templateUrl: './heather.component.html',
+  styleUrl: './heather.component.css'
+})
+export class HeatherComponent {
+
+}
